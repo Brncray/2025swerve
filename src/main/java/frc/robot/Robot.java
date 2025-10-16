f8ca14977e7509045d92e82a2efb9d5e7d563f14
awsare <57870234+Brncray@users.noreply.github.com>
2025-10-16 15:26:27 -0400

    init
    init

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,136 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+// import java.awt.Robot;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  public static final CTREConfigs ctreConfigs = new CTREConfigs();
+
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+  }
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+
+    double  x  =  m_robotContainer.driver.getRawAxis(0);
+    double  y  =  m_robotContainer.driver.getRawAxis(1);
+    double  r  =  m_robotContainer.driver.getRawAxis(4);
+    boolean Reset =  m_robotContainer.driver.getRawButton(7);   // mall Back button left of Logitech
+
+    System.out.println("" ) ;
+    System.out.println("1- x: " + x + "   y: " + y + "   r: " + r + "   Reset: " + Reset ) ;
+
+    // import frc.robot.Hardware.SwerveChassis;
+    if ( Reset )  m_robotContainer.s_Swerve.zeroHeading() ;  //   zeroHeading()  angleMotor.setPosition(0);  // swerveChassis.resetEncoders() ;
+    
+/*  THIS WAS WORKING, THEN NOT...
+    // Drive using the X, Y, and Z axes of the joystick.
+    var speeds = new ChassisSpeeds(-m_robotContainer.driver.getLeftY(), -m_robotContainer.driver.getLeftX(), 0);
+*/    
+  }
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+}
+
+
+
+/*
+
+CAN frame not received/too-stale. Check the CAN bus wiring, CAN bus utilization, and power to the device.
+
+Device firmware could not be retrieved. Check that the device is running v6 firmware, the device ID is correct, the specified CAN bus is correct, and the device is powered.
+
+CAN frame not received/too-stale. Check the CAN bus wiring, CAN bus utilization, and power to the device.
+
+Device firmware could not be retrieved. Check that the device is running v6 firmware, the device ID is correct, the specified CAN bus is correct, and the device is powered.
+
+CAN frame not received/too-stale. Check the CAN bus wiring, CAN bus utilization, and power to the device.
+
+Device firmware could not be retrieved. Check that the device is running v6 firmware, the device ID is correct, the specified CAN bus is correct, and the device is powered.
+
+*/
